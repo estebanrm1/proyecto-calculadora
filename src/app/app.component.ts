ecb99925ecbeab4acb2d21b3fbf3eb3fefb4264e
estebanrm1 <solsel90@gmail.com>
2022-08-28 13:14:09 -0300

    cambios 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto-calculadora';
+export class AppComponent implements OnInit {
+  
+  title = 'Proyecto Calculadora';
+
+  screen="";
+  a:any;
+  b:any;
+  c:any;
+  d="";
+  e=""
+
+  ngOnInit(): void {
+    
+  }
+
+  //Calculadora
+
+  enterValue(value:string){
+    if ((this.b=="+")||(this.b=="-")||(this.b=="*"||(this.b=="/"))){
+
+      this.d= this.d + value;
+      this.screen = this.screen + value;
+      this.c= this.d;
+
+    }else{
+
+      this.screen =this.screen + value;
+      this.a= this.screen;
+    }
+
+
+  }
+
+
 }
